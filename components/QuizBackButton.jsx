898effab3c1a81c497c99e68005f0f952f825885
magnuslykke <magn965m@stud.kea.dk>
2023-11-16 13:55:10 +0100

    proeve

--- components/QuizBackButton.jsx
+++ components/QuizBackButton.jsx
@@ -3,7 +3,7 @@ export default function QuizBackButton(props) {
   return (
     <div className="flex gap-4 p-2">
       <button onClick={props.clickEvent} className="p-2 outline outline-1">
-        prev
+        previous
       </button>
     </div>
   );
