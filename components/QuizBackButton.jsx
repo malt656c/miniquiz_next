eeb4d8b5a876211dec90e03ca88bd362b6fa28fc
magnuslykke <magn965m@stud.kea.dk>
2023-11-17 15:04:48 +0100

    ikoner

--- components/QuizBackButton.jsx
+++ components/QuizBackButton.jsx
@@ -3,7 +3,7 @@ export default function QuizBackButton(props) {
   return (
     <div className="flex gap-4 p-2">
       <button onClick={props.clickEvent} className="p-2  rounded-xl bg-green-beige">
-        <img src="/img/arrow.svg" alt="" />
+        <img src="/img/arrow.svg" alt="pil" />
       </button>
     </div>
   );
