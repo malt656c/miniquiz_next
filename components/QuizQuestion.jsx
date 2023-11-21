1bb0cf2761f446a236f1a3e7015542bffe1e2340
magnuslykke <magn965m@stud.kea.dk>
2023-11-21 12:06:29 +0100

    span update

--- components/QuizQuestion.jsx
+++ components/QuizQuestion.jsx
@@ -1,7 +1,7 @@
 export default function QuizQuestion(props) {
   return (
     <div>
-      <p className="p-4 bg-green-beige rounded-xl mx-8 font-headings text-2xl text-center">{props.content}</p>
+      <p className="p-4 bg-green-beige rounded-xl mx-8 mt-4 font-headings text-2xl text-center">{props.content}</p>
     </div>
   );
 }
