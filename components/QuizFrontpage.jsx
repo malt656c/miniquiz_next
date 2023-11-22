2d35cdcf86917fe299018b90cd441ea296eb7c26
Malte Jensen <malt656c@stud.kea.dk>
2023-11-22 12:46:06 +0100

    forside virker

--- components/QuizFrontpage.jsx
+++ components/QuizFrontpage.jsx
@@ -1,16 +1,20 @@
 "use client";
 export default function QuizFrontpage() {
-  let backgroundImageStyle = { backgroundImage: 'url("./img/forside.jpeg")' };
-
   return (
-    <div className="grid place-items-center bg-no-repeat bg-center object-contain h-screen w-screen" style={backgroundImageStyle}>
-      <button type="button" className="bg-green-beige font-bold rounded-lg px-5 py-2.5 text-center me-2 mb-2 mt-auto text-xl italic">
+    <div className="grid place-items-center bg-no-repeat bg-center bg-cover h-screen absolute inset-0 z-10" style={{ backgroundImage: 'url("./img/forside.jpeg")' }}>
+      <div className="bg-green-beige text-center flex flex-col justify-center p-12 gap-8 drop-shadow-md rounded-lg">
+        <h1 className="font-headings text-3xl ">Helse Quiz</h1>
+        <p className="text-lg">Find dit absolut bedste match</p>
+      </div>
+      <button
+        type="button"
+        className="bg-green-beige font-centuryGothic rounded-lg px-6 py-3 text-center text-xl transition-all hover:scale-105 hover:drop-shadow-md"
+        onClick={(e) => {
+          e.target.parentElement.remove();
+        }}
+      >
         Find dit Match
       </button>
-      <div className="bg-green-beige text-center flex flex-col justify-center pl-24 pr-24 mt-auto">
-        <h1 className="font-bold text-3xl italic mb-2 pt-20">Helse Quiz</h1>
-        <p className="pt-10 pb-20 text-lg">Find dit absolut bedste match</p>
-      </div>
     </div>
   );
 }
