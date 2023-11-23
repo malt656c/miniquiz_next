4355948fd9d52196c469b7a861a84584646756e7
Malte Jensen <malt656c@stud.kea.dk>
2023-11-23 13:55:09 +0100

    en masse sletted

--- components/QuizBackButton.jsx
+++ components/QuizBackButton.jsx
@@ -1,11 +1,11 @@
 "use client";
+import Image from "next/image";
 export default function QuizBackButton(props) {
   return (
     <div className="flex gap-4 p-2">
       <button onClick={props.clickEvent} className="p-2  rounded-xl bg-green-beige">
-        <img src="/img/arrow.svg" alt="pil" />
+        <Image src="/img/arrow.svg" alt="pil" width="27" height="16" />
       </button>
     </div>
   );
 }
-//http://localhost:3000/img/yes.svghttp://localhost:3000/img/yes.svghttp://localhost:3000/img/yes.svg
